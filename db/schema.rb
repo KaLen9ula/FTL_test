4260b57c685b4baffb405f9c07b1bcc572025763
Kalen9ula <andruhovickola167@gmail.com>
2022-08-07 15:28:21 +0300

    modify db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_31_122834) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_04_064905) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_31_122834) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "amount"
     t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
