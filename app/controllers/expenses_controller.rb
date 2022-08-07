6327939ba29e0686ba9f395f8cfe878497eeba37
Kalen9ula <andruhovickola167@gmail.com>
2022-08-07 15:22:16 +0300

    modify controllers

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,9 +1,11 @@
 class ExpensesController < ApplicationController
-  before_action :set_expense
+  before_action :set_expense, only: [:show, :edit, :update, :destroy]
+  helper_method :sort_column, :sort_direction
 
   def index
     @current_user_expenses = Expense.all.accessible_for(current_user)
-    @expenses = @current_user_expenses.order(sort_col + '' + sort_dir)
+    @expenses = @current_user_expenses.order(sort_column + ' ' + sort_direction)
+    render_xhr
   end
 
   def show
@@ -17,13 +19,18 @@ class ExpensesController < ApplicationController
   end
 
   def create
-    @expense = Expense.new(expense_params)
-    redirect_to expense_path(expense)
+    @expense = Expense.create(expense_params)
+    redirect_to expense_path(@expense)
+  end
+
+  def update
+    @expense.update(expense_params)
+    redirect_to expense_path(@expense)
   end
 
   def destroy
     @expense.destroy
-
+    redirect_to root_path
   end
 
   private
@@ -33,14 +40,18 @@ class ExpensesController < ApplicationController
   end
 
   def expense_params
-    params[:expense].permit(:title, :description, :category, :user_id, :created_at)
+    params[:expense].permit(:title, :description, :category, :user_id, :amount, :created_at)
   end
 
-  def sort_col
+  def sort_column
     params[:sort] || 'created_at'
   end
 
-  def sort_dir
+  def sort_direction
     params[:direction] || 'desc'
+  end
+
+  def render_xhr
+    render layout: false if request.xhr?
   end
 end
